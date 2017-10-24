dee4c3513937eafd56554fda368b93661f44bb8b
Max Goshay <ghanzo@gmail.com>
2017-10-24 14:39:28 -0700

    Update lab6.cpp

--- lab6.cpp
+++ lab6.cpp
@@ -1,4 +1,5 @@
-
+// Programmer: Max Goshay
+// Programmer's ID: 252053
 #define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
 #include <string>
@@ -10,148 +11,38 @@
 using namespace std;
 
 template <typename V>
-class DynamicArray
+class Stack
 {
 	int cap;
+	int siz;
 	V dummy;
 	V* values;
+	void capacity(int);
+	
 public:
-	DynamicArray(int = 2); // default constructor
-	DynamicArray(const DynamicArray<V>&); // copy c
-	~DynamicArray() { delete[] values; } //destructor
-	DynamicArray<V>& operator=(const DynamicArray<V>&);
-	int capacity() const { return cap; }
-	void capacity(int); // setter cap
-	V operator [] (int) const; //getter
-	V& operator [] (int); //setter
-};
-//constructor
-template <typename V>
-DynamicArray<V>::DynamicArray(int cap)
-{
-	this->cap = cap;
-	values = new V[cap];
-	for (int i = 0; i < cap; i++) {
-		values[i] = V();
-	}
-}
-//copy constructor
-template <typename V>
-DynamicArray<V>::DynamicArray(const DynamicArray<V> & original) {
-	cap = original.cap;
-	values = new V[cap][1];
-	dummy = V();
-	for (int i = 0; i < cap; i++)
-		values[i] = original.values[i];
-	dummy = original.dummy;
-}
-//operator =
-template <typename V>
-DynamicArray<V>& DynamicArray<V>::operator=(const DynamicArray<V>&
-	original) {
-	if (this != &original)
-	{
-		delete[] values;
-		cap = original.cap;
-		values = new V[cap];
-		for (int i = 0; i < cap; i++)
-			values[i] = original.values[i];
-		dummy = original.dummy;
-	}
-	return *this;
-}
-//set capacity
-template <typename V>
-void DynamicArray<V>::capacity(int cap) {
-	V* temp = new V[cap];
-	int limit = min(cap, this->cap);
-	for (int i = 0; i < limit; i++)
-		temp[i] = values[i];
-	for (int i = limit; i < cap; i++)
-		temp[i] = V();
-	delete[] values;
-	values = temp;
-	this->cap = cap;
-}
-//getter []
-template <typename V>
-V DynamicArray<V>::operator[ ](int index) const
-{
-	if (index < 0) return dummy;
-	if (index >= cap) return dummy;
-	return values[index];
-}
-//setter []
-template <typename V>
-V& DynamicArray<V>::operator[ ](int index)
-{
-	if (index < 0) return dummy;
-	if (index >= cap) capacity(index * 2);
-	return values[index];
-}
-
-//|||||||||||||||||||||||||||||||||||||||||||||||||
-
-//|||||||||||||||||||||||||||||||||||||||||||||||||
-
-//|||||||||||||||||||||||||||||||||||||||||||||||||
-
-template <typename V>
-class Stack {
-  private:
-  int cap;
-  int size;
-	V dummy;
-	V* values;
-  void capacity(int);
-  
-  public:
-  Stack(); // may have a defaulted parameter
-  Stack(const Stack<V>&); // copy constructor
-  ~Stack(){ delete[] values; }
-  Stack<V>& operator=(const Stack<V>&);
-  void push(const V&);
-  V& peek(){ return values[size-1];}
-  void pop(){ if (!empty())size--;}
-  int size() const { return size; }
-  bool empty() const {if (size == 0) return true;}
-  void clear(){size = 0;}
+	Stack(int = 2); // default constructor
+	Stack(const Stack<V>&); // copy c
+	~Stack() { delete[] values; } //destructor
+	Stack<V>& operator=(const Stack<V>&);
+	void push(const V&);
+	void pop();
+	V& peek(){ return values[siz-1];}
+	bool empty();
+  int size() const { return siz; }
+  int capacity() const { return cap; }
 };
 
+//push
 template <typename V>
 void Stack<V>::push(const V& val){
-  if (cap == size)
-  capacity(size+1);
-  if (cap > size)
-  values[size] = val;
-  size++;
+  if (cap == siz)
+  capacity(siz*2);
+  if (cap > siz)
+  values[siz] = val;
+  siz++;
 }
 
-//constructor
-template <typename V>
-Stack<V>::Stack(int cap)
-{
-	this->cap = cap;
-	size = 0;
-	values = new V[cap];
-	for (int i = 0; i < cap; i++) {
-		values[i] = V();
-	}
-}
-
-//copy constructor
-template <typename V>
-Stack<V>::Stack(const Stack<V> & original) {
-  this.size = original.size;
-	cap = original.cap;
-	values = new V[cap][1];
-	dummy = V();
-	for (int i = 0; i < cap; i++)
-		values[i] = original.values[i];
-	dummy = original.dummy;
-}
-
-//set capacity
+//set cap private function
 template <typename V>
 void Stack<V>::capacity(int cap) {
 	V* temp = new V[cap];
@@ -165,6 +56,40 @@ void Stack<V>::capacity(int cap) {
 	this->cap = cap;
 }
 
+//pop
+template <typename V>
+void Stack<V>::pop(){
+ if (siz > 0)siz--;
+}
+
+//is empty?
+template <typename V>
+bool Stack<V>::empty(){
+ if (siz == 0) return true;
+}
+
+//constructor
+template <typename V>
+Stack<V>::Stack(int cap)
+{
+	this->cap = cap;
+	siz = 0;
+	values = new V[cap];
+	for (int i = 0; i < cap; i++) {
+		values[i] = V();
+	}
+}
+//copy constructor
+template <typename V>
+Stack<V>::Stack(const Stack<V> & original) {
+	cap = original.cap;
+	size = original.size;
+	values = new V[cap][1];
+	dummy = V();
+	for (int i = 0; i < cap; i++)
+		values[i] = original.values[i];
+	dummy = original.dummy;
+}
 //operator =
 template <typename V>
 Stack<V>& Stack<V>::operator=(const Stack<V>&
@@ -172,7 +97,7 @@ Stack<V>& Stack<V>::operator=(const Stack<V>&
 	if (this != &original)
 	{
 		delete[] values;
-		this.size = original.size;
+		siz = original.siz;
 		cap = original.cap;
 		values = new V[cap];
 		for (int i = 0; i < cap; i++)
@@ -182,11 +107,46 @@ Stack<V>& Stack<V>::operator=(const Stack<V>&
 	return *this;
 }
 
-int main()
-{
-  Stack <int> test1(10);
+void operatorParser(Stack<double> &kobe, Stack<char> &kober){
+  while (kober.size() > 0){
+        int value1 = kobe.peek();
+        kobe.pop();
+        int value2 = kobe.peek();
+        kobe.pop();
+    switch(kober.peek()) {
+      case '+' : 
+        cout << value1+value2 << "\t= " << value1 << "\t+" << value2 << "\n";
+        break;
+      case '/' : cout << value2/value1 << "\t= " << value2 << "\t/" << value1 << "\n";
+        break;
+      case '-' : 
+        cout << value2-value1 << "\t= " << value2 << "\t-" << value1 << "\n";
+        break;
+      case '*' :
+        cout << value2*value1 << "\t= " << value2 << "\t*" << value1 << "\n";
+        break;
+    }
+        kober.pop();
+  }
+};
 
+int main (){
+  Stack<double> kobe;
+  Stack<char> kober;
   
+  cout << "Input values for calculations\n";
+  for (int i = 1; i < 7; i++ ) {
+    if (i % 3 == 0){
+      char operation;  
+      cin >> operation;
+      kober.push(operation);
+      continue;
+    }
+    double integer;
+    cin >> integer;
+    kobe.push(integer);
+  }
+  operatorParser(kobe, kober);
   
-	return 0;
+  return 0;
 }
